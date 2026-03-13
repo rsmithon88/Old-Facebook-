743c0db8477a7a6cea466aec1a5583bb59792360
rsmithon88 <105262364+rsmithon88@users.noreply.github.com>
2026-03-13 19:08:00 +0600

    Update Old.py

--- Old.py
+++ Old.py
@@ -1,5 +1,4 @@
 import os, platform, time, sys
-
 os.system('xdg-open https://chat.whatsapp.com/DFj8atKbJZYE1zc68YMnvK?mode=gi_t')
 time.sleep(2)
 
@@ -8,25 +7,17 @@ try:
 except:
     os.system("pip uninstall requests -y")
     os.system("pip install requests")
-
 os.system('git pull --quiet 2>/dev/null')
 
 bit = platform.architecture()[0]
-
 if bit == '64bit':
     print('\033[1;97m[✓] \033[1;92mFound 64 Bit Device')
-
-    try:
-        import old   # এটা old.cpython-312.so কে run করবে
-    except Exception as e:
-        print("Module load error:", e)
-        sys.exit()
-
+    import old
 elif bit == '32bit':
     print('\033[1;97m[✓] \033[1;91mSORRY BRO YOUR DEVICE IS 32 BIT')
     print('\033[1;97m[✓] \033[1;91mPLEASE USE 64 BIT DEVICE')
     sys.exit()
-
 else:
     print('\033[1;97m[!] \033[1;91mUnknown Architecture')
     sys.exit()
+
